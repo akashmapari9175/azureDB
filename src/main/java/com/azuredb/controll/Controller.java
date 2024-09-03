a8a9d5ac2c1f62c27fa944a41113e9275ebfe60d
akashmapari9175 <itsakashmapari@gmail.com>
2024-09-03 16:30:52 +0530

    add getMethod

--- src/main/java/com/azuredb/controll/Controller.java
+++ src/main/java/com/azuredb/controll/Controller.java
@@ -2,9 +2,11 @@ package com.azuredb.controll;
 
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,6 +37,11 @@ public class Controller {
 		return employee;
 	}
 	
+	@GetMapping("/emp/{id}")
+	public Employee getEmp(@PathVariable int id) {
+		Employee emp = repo.findById(id).get();
+		return emp;
+	}
 	
 	
 }
